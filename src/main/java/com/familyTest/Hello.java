0e92ae801665197c27778bbe942d29af9add6b0e
Like Free <likefreefree@gmail.com>
2021-10-12 17:24:22 -0400

    conflict

--- src/main/java/com/familyTest/Hello.java
+++ src/main/java/com/familyTest/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
   public static void main(String[] args) {
 	System.out.println("hello");//1
 	System.out.println("hello");//2
-	System.out.println("hello");//3
+	System.out.println("hello");////modify by gitTest1
 }
 }
