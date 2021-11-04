3eab08a2a461ca279d79b7cf606daa41ab71001e
Like Free <likefreefree@gmail.com>
2021-11-04 12:45:20 -0400

    test hotfix

--- src/main/java/com/familyTest/Hello.java
+++ src/main/java/com/familyTest/Hello.java
@@ -3,7 +3,7 @@ package com.familyTest;
 public class Hello {
   public static void main(String[] args) {
 	System.out.println("hello");//1
-	System.out.println("hello");//2
+	System.out.println("hello");//2 edit by hotfix
 	System.out.println("hello");////modify by gitTest1
 	System.out.println("hello");//modify by gitTest
 }
